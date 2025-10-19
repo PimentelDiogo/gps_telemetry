1282514c32f6585eacf1b295bcebbd31128c0844
Diogo <diogo.oliveira@bitka.com.br>
2025-10-19 15:54:08 -0300

    build: update Dart SDK and dependencies to 3.35.6
    Update Dart SDK version from 3.27.4 to 3.35.6 and refresh package dependencies
    Add web plugin registrations for geolocator, maps, permissions and sensors
    Remove deprecated package config files

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,6 +3,24 @@
 // Generated file. Do not edit.
 //
 
+// @dart = 2.13
 // ignore_for_file: type=lint
 
-void registerPlugins() {}
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:sensors_plus/src/sensors_plus_web.dart';
+import 'package:share_plus/src/share_plus_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  GeolocatorPlugin.registerWith(registrar);
+  GoogleMapsPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  WebSensorsPlugin.registerWith(registrar);
+  SharePlusWebPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
